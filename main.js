17542007d28c748f51b088e7d25a08a6f1e68902
2DavidGarcia4 <gacub20ru@gmail.com>
2022-07-09 20:37:07 -0500

    start

---
+++ main.js
@@ -0,0 +1,27 @@
+const list = document.querySelector(".menu")
+const menu = document.querySelector(".icon_menu")
+const equis = document.querySelector(".icon_equis")
+
+menu.addEventListener("click", ()=>{
+   list.classList.toggle("menu_barr")
+})
+equis.addEventListener("click", ()=>{
+   list.classList.toggle("menu_barr")
+})
+
+const elemntsList = document.querySelectorAll(".menu li")
+const elemntsListA = document.querySelectorAll(".menu li a")
+console.log(elemntsList)
+elemntsListA.forEach((item, psi) => {
+   item.addEventListener("click", () => {
+      elemntsListA.forEach((item2, psi2) => {
+         elemntsList[psi2].classList.remove("active")
+      })
+      elemntsList[psi].classList.add("active")
+   })
+})
+
+const nav = document.querySelector(".header nav")
+document.addEventListener("scroll", () => {
+   nav.classList.toggle("style", window.scrollY > 140)
+})
